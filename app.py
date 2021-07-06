b4b9f17581d92a7767f2f1b927060b818271d4a6
Daniel Sikar <dsikar@gmail.com>
2021-07-06 23:44:47 +0100

    Writing file to S3

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ import tensorflow as tf
 
 s3 = boto3.resource('s3')
 client_s3 = boto3.client('s3')
-# copy tensorflow trained model to local filesystems
 result = client_s3.download_file("dsikar.models.bucket",'firemodel.h5', "/tmp/firemodel.h5")
 model = tf.keras.models.load_model('/tmp/firemodel.h5')
-# define image size and categories
 plot_size = 64
 IMAGE_SHAPE = (plot_size, plot_size)
 CATEGORIES = ['nofire', 'fire']
@@ -20,8 +18,11 @@ def lambda_handler(event, context):
   img = readImageFromBucket(key, bucket_name).resize(IMAGE_SHAPE)
   img = np.array(img)/255.0
   prediction = model.predict(np.expand_dims(img, axis=0))[0]
-  predicted_class = CATEGORIES[np.argmax(prediction)]
+  predicted_class = CATEGORIES[np.argmax(prediction)] 
   print('ImageName: {0}, Prediction: {1}'.format(key, predicted_class))
+  response_str = 'ImageName: {0}, Prediction: {1}'.format(key, predicted_class)
+  filename = key + '.txt'
+  client_s3.put_object(Body=response_str, Bucket='tensorflow-images-predictions-dsikar', Key=filename)
 
 def readImageFromBucket(key, bucket_name):
   bucket = s3.Bucket(bucket_name)
